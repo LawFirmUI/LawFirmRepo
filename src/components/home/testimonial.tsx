603ee3ee99efafcd6779c4b0ec9f03b13512c2c9
Jessie-Asante <kaprikon1995@gmail.com>
2023-10-05 15:26:19 +0100

    changed

--- src/components/home/testimonial.tsx
+++ src/components/home/testimonial.tsx
@@ -119,7 +119,7 @@ const HomeTestimonial: FC = () => {
           </Grid>
           <Grid item xs={12} md={6} sx={{ display: { xs: 'none', md: 'block' } }}>
             <Box sx={{ width: { xs: '100%', md: '90%' } }}>
-              <Image src="/images/testimony.jpeg" width={520} height={540} quality={97} alt="Testimonial img" />
+              <Image src="/images/testimony1.png" width={520} height={540} quality={97} alt="Testimonial img" />
             </Box>
           </Grid>
         </Grid>
